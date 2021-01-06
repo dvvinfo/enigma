88fb5cc9e5cbca2fe8a972963e2318daa62d0f24
Vladimir Drobyshev <dvvinfo@mail.ru>
2021-01-06 18:32:57 +0300

    Made up the Courses page desktop and mobile versions

--- src/js/main.js
+++ src/js/main.js
@@ -68,5 +68,38 @@ $(document).ready(function(){
 				prevArrow:'<button type="button" class="slick-prev"> <img src="../images/Arrow-prev.svg" alt="" class="slider-prev"></button>',
 				nextArrow:'<button type="button" class="slick-next"><img src="../images/Arrow-next.svg" alt="" class="slider-next"></button>'
 			});
+			$('.courses__slider').slick({
+				infinite: true,
+  				slidesToShow: 3,
+				slidesToScroll: 1,
+				dots: true,
+				prevArrow:'<button type="button" class="slick-prev"> <img src="../images/Arrow-prev.svg" alt="" class="slider-prev"></button>',
+				nextArrow:'<button type="button" class="slick-next"><img src="../images/Arrow-next.svg" alt="" class="slider-next"></button>',
+				responsive: [
+					{
+					  breakpoint: 1024,
+					  settings: {
+						slidesToShow: 2,
+						slidesToScroll: 1,
+						infinite: true,
+						dots: true
+					  }
+					},
+					
+					
+				  ]
+			});
+
+
+			$('.courses__slider-mobi').slick({
+				infinite: true,
+  				slidesToShow: 1,
+				slidesToScroll: 1,
+				dots: true,
+				arrows:false,
+				prevArrow:'<button type="button" class="slick-prev"> <img src="../images/Arrow-prev.svg" alt="" class="slider-prev"></button>',
+				nextArrow:'<button type="button" class="slick-next"><img src="../images/Arrow-next.svg" alt="" class="slider-next"></button>',
+				
+			});
 		
 });
